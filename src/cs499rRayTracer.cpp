d0ca74c599b1ecea447bd672adc0ed6c1d459808
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-09-23 12:57:04 -0400

    Implements CS499R::RayTracer's destructor

--- src/cs499rRayTracer.cpp
+++ src/cs499rRayTracer.cpp
@@ -7,7 +7,7 @@ namespace CS499R
 
     RayTracer::RayTracer(cl_device_id const & device)
     {
-        cl_int error;
+        cl_int error = 0;
 
         mContext = clCreateContext(0, 1, &device, 0, 0, &error);
 
@@ -20,7 +20,12 @@ namespace CS499R
 
     RayTracer::~RayTracer()
     {
+        cl_int error = 0;
 
+        error |= clReleaseCommandQueue(mCmdQueue);
+        error |= clReleaseContext(mContext);
+
+        CS499R_ASSERT(error == 0);
     }
 
 }
