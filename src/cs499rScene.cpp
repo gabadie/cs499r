d4bb29199b7f2171e4297c92c0789cc398d98b7a
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-09-23 20:12:12 -0400

    Implements CS499R::Scene's bones

---
+++ src/cs499rScene.cpp
@@ -0,0 +1,17 @@
+
+#include "cs499rScene.hpp"
+
+namespace CS499R
+{
+
+    Scene::Scene()
+    {
+
+    }
+
+    Scene::~Scene()
+    {
+
+    }
+
+}
---
+++ src/cs499rScene.hpp
@@ -0,0 +1,33 @@
+
+#ifndef _H_CS499R_SCENE
+#define _H_CS499R_SCENE
+
+#include "cs499rPrefix.hpp"
+
+
+namespace CS499R
+{
+
+    /*
+     * The scene old all the scene information on the CPU side
+     */
+    class Scene
+    {
+    public:
+        // --------------------------------------------------------------------- IDLE
+
+        Scene();
+        ~Scene();
+
+
+    private:
+        // --------------------------------------------------------------------- MEMBERS
+
+
+
+
+    };
+
+}
+
+#endif // _H_CS499R_SCENE
