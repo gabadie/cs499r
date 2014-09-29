862d040c6a0a81db411a1d2c4cb41572069355ff
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-09-29 19:31:55 -0400

    Moves kernel_path_tracer_main's rebound ray generator into path_tracer_rebound()

--- src/cs499rRayTracerProgram.cpp
+++ src/cs499rRayTracerProgram.cpp
@@ -261,6 +261,48 @@ namespace
             sampleCx->raySceneDirection = normalize(rayFocusPoint - sampleCx->raySceneOrigin);
         }
 
+        inline
+        void
+        path_tracer_rebound(
+            sample_context_t * const sampleCx
+        )
+        {
+            sampleCx->raySceneOrigin += sampleCx->raySceneDirection * sampleCx->rayInterDistance;
+
+            __global common_mesh_instance_t const * const meshInstance = sampleCx->rayInterMeshInstance;
+
+            float32x3_t const meshNormal = normalize(sampleCx->rayInterMeshNormal);
+            float32x3_t const n = (
+                meshInstance->meshSceneMatrix.x * meshNormal.x +
+                meshInstance->meshSceneMatrix.y * meshNormal.y +
+                meshInstance->meshSceneMatrix.z * meshNormal.z
+            );
+
+            float32x3_t u;
+
+            if (fabs(n.x) > fabs(n.y))
+            {
+                u = (float32x3_t)(-n.z, 0.0f, n.x);
+            }
+            else
+            {
+                u = (float32x3_t)(0.0f, -n.z, n.y);
+            }
+
+            float32x3_t const u2 = u * u;
+            float32_t const r2 = random(sampleCx);
+            float32_t const r2s = sqrt(r2) * rsqrt(u2.x + u2.y + u2.z);
+
+            float32x3_t const v = cross(n, u);
+            float32_t const r1 = 2.0f * kPi * random(sampleCx);
+
+            sampleCx->raySceneDirection = (
+                u * (cos(r1) * r2s) +
+                v * (sin(r1) * r2s) +
+                n * sqrt(1.0f - r2)
+            );
+        }
+
     );
 
     /*
@@ -318,43 +360,7 @@ namespace
 
             for (uint32_t i = 0; i < kRecursionCount; i++)
             {
-                { // generate a new difuse ray
-                    sampleCx.raySceneOrigin += sampleCx.raySceneDirection * sampleCx.rayInterDistance;
-
-                    __global common_mesh_instance_t const * const meshInstance = sampleCx.rayInterMeshInstance;
-
-                    float32x3_t const meshNormal = normalize(sampleCx.rayInterMeshNormal);
-                    float32x3_t const n = (
-                        meshInstance->meshSceneMatrix.x * meshNormal.x +
-                        meshInstance->meshSceneMatrix.y * meshNormal.y +
-                        meshInstance->meshSceneMatrix.z * meshNormal.z
-                    );
-
-                    float32x3_t u;
-
-                    if (fabs(n.x) > fabs(n.y))
-                    {
-                        u = (float32x3_t)(-n.z, 0.0f, n.x);
-                    }
-                    else
-                    {
-                        u = (float32x3_t)(0.0f, -n.z, n.y);
-                    }
-
-                    float32x3_t const u2 = u * u;
-                    float32_t const r2 = random(&sampleCx);
-                    float32_t const r2s = sqrt(r2) * rsqrt(u2.x + u2.y + u2.z);
-
-                    float32x3_t const v = cross(n, u);
-                    float32_t const r1 = 2.0f * kPi * random(&sampleCx);
-
-                    sampleCx.raySceneDirection = (
-                        u * (cos(r1) * r2s) +
-                        v * (sin(r1) * r2s) +
-                        n * sqrt(1.0f - r2)
-                    );
-                }
-
+                path_tracer_rebound(&sampleCx);
                 scene_intersection(&sampleCx, shotCx, meshInstances, primitives);
 
                 sampleColor += sampleCx.rayInterMeshInstance->emitColor * sampleColorMultiply;
