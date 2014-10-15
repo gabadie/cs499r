ebcddd30ce76ef9a5602d63b0a51e9774474f555
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-10-14 21:45:27 -0400

    Modifies testing scene with more lights

--- src/appScene.cpp
+++ src/appScene.cpp
@@ -2,21 +2,20 @@
 #include "app.hpp"
 
 
-namespace App
+namespace
 {
+    float32_t const kRoomWidth = 20.0f;
+    float32_t const kRoomHeight = 10.0f;
+
+    float32x3_t const kColorRed(0.75f, 0.25f, 0.25f);
+    float32x3_t const kColorBlue(0.25f, 0.25f, 0.75f);
+    float32x3_t const kColorWhite(0.75f, 0.75f, 0.75f);
 
     void
-    buildSceneMeshes(CS499R::Scene & scene)
+    buildSceneRoom(CS499R::Scene & scene)
     {
-        float32_t const radius = 20.0f;
-        float32_t const height = 10.0f;;
-        float32_t const lightSize = 9.0f;
-
-        float32x3_t const colorRed(0.75f, 0.25f, 0.25f);
-        float32x3_t const colorBlue(0.25f, 0.25f, 0.75f);
-        float32x3_t const colorWhite(0.75f, 0.75f, 0.75f);
-        float32x3_t const colorLight(5.0f);
-
+        float32_t const radius = kRoomWidth;
+        float32_t const height = kRoomHeight;
 
         { // Room's walls
             float32x3_t meshVertices[] = {
@@ -35,7 +34,7 @@ namespace App
             {
                 auto sceneMeshInstance = scene.addMeshInstance("room/wall/x-", sceneMesh);
 
-                sceneMeshInstance->mColorDiffuse = colorRed;
+                sceneMeshInstance->mColorDiffuse = kColorRed;
                 sceneMeshInstance->mScenePosition = float32x3_t(-radius, 0.0f, 0.0f);
                 sceneMeshInstance->mMeshSceneMatrix = float32x3x3_t(
                     float32x3_t(+1.0f, 0.0f, 0.0f),
@@ -47,7 +46,7 @@ namespace App
             {
                 auto sceneMeshInstance = scene.addMeshInstance("room/wall/y-", sceneMesh);
 
-                sceneMeshInstance->mColorDiffuse = colorWhite;
+                sceneMeshInstance->mColorDiffuse = kColorWhite;
                 sceneMeshInstance->mScenePosition = float32x3_t(0.0f, -radius, 0.0f);
                 sceneMeshInstance->mMeshSceneMatrix = float32x3x3_t(
                     float32x3_t(0.0f, +1.0f, 0.0f),
@@ -59,7 +58,7 @@ namespace App
             {
                 auto sceneMeshInstance = scene.addMeshInstance("room/wall/x+", sceneMesh);
 
-                sceneMeshInstance->mColorDiffuse = colorBlue;
+                sceneMeshInstance->mColorDiffuse = kColorBlue;
                 sceneMeshInstance->mScenePosition = float32x3_t(+radius, 0.0f, 0.0f);
                 sceneMeshInstance->mMeshSceneMatrix = float32x3x3_t(
                     float32x3_t(-1.0f, 0.0f, 0.0f),
@@ -71,7 +70,7 @@ namespace App
             {
                 auto sceneMeshInstance = scene.addMeshInstance("room/wall/y+", sceneMesh);
 
-                sceneMeshInstance->mColorDiffuse = colorWhite;
+                sceneMeshInstance->mColorDiffuse = kColorWhite;
                 sceneMeshInstance->mScenePosition = float32x3_t(0.0f, +radius, 0.0f);
                 sceneMeshInstance->mMeshSceneMatrix = float32x3x3_t(
                     float32x3_t(0.0f, -1.0f, 0.0f),
@@ -113,33 +112,85 @@ namespace App
             scene.addMeshInstance("room/roof", sceneMesh);
         }
 
-        { // light
-            float32x3_t vl0 { radius - lightSize, radius, height };
-            float32x3_t vl1 { radius, radius, height - lightSize };
-            float32x3_t vl2 { radius, radius - lightSize, height };
+    }
 
-            float32x3_t meshVertices[] = {
-                vl0,
-                vl1,
-                vl2,
-            };
+    void
+    buildSceneLights(CS499R::Scene & scene)
+    {
+        float32_t const kLightSize = 9.0f;
+        float32x3_t const kLightColor = 5.0f;
 
-            CS499R::Mesh mesh(CS499R_ARRAY_SIZE(meshVertices), meshVertices);
+        float32x3_t const vl0 { kRoomWidth - kLightSize, kRoomWidth, kRoomHeight };
+        float32x3_t const vl1 { kRoomWidth, kRoomWidth, kRoomHeight - kLightSize };
+        float32x3_t const vl2 { kRoomWidth, kRoomWidth - kLightSize, kRoomHeight };
 
-            auto sceneMesh = scene.addMesh("room/light", mesh);
-            auto sceneMeshInstance = scene.addMeshInstance("room/light", sceneMesh);
+        float32x3_t const meshVertices[] = {
+            vl0,
+            vl1,
+            vl2,
+        };
 
-            sceneMeshInstance->mColorDiffuse = colorLight;
-            sceneMeshInstance->mColorEmit = colorLight;
+        CS499R::Mesh mesh(CS499R_ARRAY_SIZE(meshVertices), meshVertices);
+
+        auto sceneMesh = scene.addMesh("room/light", mesh);
+
+        {
+            auto sceneMeshInstance = scene.addMeshInstance("room/light/001", sceneMesh);
+
+            sceneMeshInstance->mColorDiffuse = 0.0f;
+            sceneMeshInstance->mColorEmit = kLightColor;
         }
 
+        {
+            auto sceneMeshInstance = scene.addMeshInstance("room/light/002", sceneMesh);
+
+            sceneMeshInstance->mColorDiffuse = 0.0f;
+            sceneMeshInstance->mColorEmit = kLightColor * 2.0f;
+
+            sceneMeshInstance->mMeshSceneMatrix = float32x3x3_t(
+                float32x3_t(0.0f, -1.0f, 0.0f),
+                float32x3_t(+1.0f, 0.0f, 0.0f),
+                float32x3_t(0.0f, 0.0f, +1.0f)
+            );
+        }
+
+        {
+            auto sceneMeshInstance = scene.addMeshInstance("room/light/003", sceneMesh);
+
+            sceneMeshInstance->mColorDiffuse = 0.0f;
+            sceneMeshInstance->mColorEmit = kLightColor;
+
+            sceneMeshInstance->mMeshSceneMatrix = float32x3x3_t(
+                float32x3_t(-1.0f, 0.0f, 0.0f),
+                float32x3_t(0.0f, -1.0f, 0.0f),
+                float32x3_t(0.0f, 0.0f, +1.0f)
+            );
+        }
+
+        {
+            auto sceneMeshInstance = scene.addMeshInstance("room/light/004", sceneMesh);
+
+            sceneMeshInstance->mColorDiffuse = 0.0f;
+            sceneMeshInstance->mColorEmit = kLightColor;
+
+            sceneMeshInstance->mMeshSceneMatrix = float32x3x3_t(
+                float32x3_t(0.0f, +1.0f, 0.0f),
+                float32x3_t(-1.0f, 0.0f, 0.0f),
+                float32x3_t(0.0f, 0.0f, +1.0f)
+            );
+        }
+    }
+
+    void
+    buildSceneContent(CS499R::Scene & scene)
+    {
         { // Monkey
             CS499R::Mesh mesh("models/monkey.obj");
 
             auto sceneMesh = scene.addMesh("monkey", mesh);
             auto sceneMeshInstance = scene.addMeshInstance("monkey/001", sceneMesh);
 
-            sceneMeshInstance->mColorDiffuse = colorWhite;
+            sceneMeshInstance->mColorDiffuse = kColorWhite;
             sceneMeshInstance->mScenePosition = float32x3_t(-0.0f, -0.0f, 2.0f);
             sceneMeshInstance->mMeshSceneMatrix = float32x3x3_t(
                 float32x3_t(0.0f, +1.0f, 0.0f),
@@ -147,6 +198,19 @@ namespace App
                 float32x3_t(0.0f, 0.0f, +1.0f)
             );
         }
+    }
+
+}
+
+namespace App
+{
+
+    void
+    buildSceneMeshes(CS499R::Scene & scene)
+    {
+        buildSceneRoom(scene);
+        buildSceneLights(scene);
+        buildSceneContent(scene);
     }
 
 }
